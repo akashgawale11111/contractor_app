1c528cdda14c8fa9a367156105863f8a49aa0e67
akashgawale11111 <akashgawale027@gmail.com>
2025-10-13 16:49:08 +0530

    images

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.contractor_app"
+    namespace = "com.mmprecise.contractorapps"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.contractor_app"
+        applicationId = "com.mmprecise.contractorapps"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/mmprecise/contractorapps/MainActivity.kt
+++ android/app/src/main/kotlin/com/mmprecise/contractorapps/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.contractor_app
+package com.mmprecise.contractorapps
 
 import io.flutter.embedding.android.FlutterActivity
 
